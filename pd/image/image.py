d9cf8bf6f9e5f02381e3301e43a0ffd2f51d49c8
mickeyshaughnessy <mickeyshaughnessy@gmail.com>
2024-06-18 13:04:45 -0600

    update main

--- pd/image/image.py
+++ pd/image/image.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 
-class Drawer():
-
-    def __init__(self, magnify=False):
+class Drawer:
+    def __init__(self, magnify=1):
         cv2.namedWindow("strategies")
         cv2.namedWindow("reputations")
         cv2.namedWindow("totals")
@@ -13,19 +12,17 @@ class Drawer():
         if norm:
             _max, _min = np.amax(img), np.amin(img)
             if _max > 0.0:
-                img = np.divide(img, _max)
-        if not self.magnify:
-            cv2.imshow(_type, img)
-        else:
-            cv2.imshow(_type, np.kron(img, np.ones((self.magnify, self.magnify))))
+                img = img / _max
+        if self.magnify > 1:
+            img = np.kron(img, np.ones((self.magnify, self.magnify)))
+        cv2.imshow(_type, img)
         cv2.waitKey(1)
 
-
 if __name__ == "__main__":
-    import numpy as np
-    pix = np.random.rand(200,200) 
+    pix = np.random.rand(200, 200)
     while True:
-        dp = (np.random.rand(200, 200) - 0.5 ) / 2.0
+        dp = (np.random.rand(200, 200) - 0.5) / 2.0
         pix = pix + dp
         cv2.imshow('image', pix)
         cv2.waitKey(1)
+
